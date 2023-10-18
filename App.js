fb2231224b5695430f3c0b731a0ff4e790255fbd
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-10-18 18:53:03 -0300

    workspace flexbox, background image

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import hotBackground from './assets/hot.png';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={s.root}>
-        <Text>Hello</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground style={s.backgroundImg} source={hotBackground}>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.root}>
+          <View style={s.workspace}>
+            <Text>Temperature</Text>
+            <Text>Input</Text>
+            <Text>Button</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
   );
 }
 
 const s = StyleSheet.create({
   root: {
     flex: 1,
+    justifyContent: "center"
+  },
+  workspace: {
+    height: 450,
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  backgroundImg: {
+    height: "100%",
   }
 });
